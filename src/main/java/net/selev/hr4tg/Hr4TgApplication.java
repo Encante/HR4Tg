df8a5b4759c2e6d17458d212ee2431ef60cc261e
Michal <encatee@gmail.com>
2025-06-10 00:28:21 +0200

    CI/CD try 3

--- src/main/java/net/selev/hr4tg/Hr4TgApplication.java
+++ src/main/java/net/selev/hr4tg/Hr4TgApplication.java
@@ -16,7 +16,7 @@ public class Hr4TgApplication {
         ApplicationContext ctx = SpringApplication.run(Hr4TgApplication.class, args);
         Secrets secrets = ctx.getBean(Secrets.class);
         RestClient rc = ctx.getBean(RestClient.class);
-        SendMessage msg = new SendMessage(secrets.JA_CHATID,"Hello world with CI/CD!!! Try me!");
+        SendMessage msg = new SendMessage(secrets.JA_CHATID,"Hello world with CI/CD!!! Try me2!");
         Response response = rc.standardTgJsonPost("sendmessage", msg);
         log.info("Send Message {}",response.toString());
     }
